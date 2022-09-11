782ea9e037f5327bcff4060050cc1e64fe074369
Muhammad Nurwibawanto <42857586+anto1290@users.noreply.github.com>
2022-09-11 17:15:53 +0700

    change name for cv

--- pages/cv.jsx
+++ pages/cv.jsx
@@ -8,7 +8,7 @@ const Cv = () => {
       <div className="w-full h-full py-16 px-2">
         <div className="grid grid-cols-8 gap-8">
           <div className='col-span-8'>
-            <iframe src="/muhmmad_nurwibawanto.pdf" style={{ width: '100%', height: '800px' }}>
+            <iframe src="/muhammad_nurwibawanto.pdf" style={{ width: '100%', height: '800px' }}>
             </iframe>
           </div>
         </div>
