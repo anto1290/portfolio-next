9735ea919d11b27175fa014a519baa22ca737b53
muhammad nurwibawanto <mnurwibawnato@gmail.com>
2022-09-11 15:55:28 +0700

    initial portfolio

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import React from 'react'
 import About from '../components/About'
 import Contact from '../components/Contact'
@@ -9,6 +10,10 @@ import Skills from '../components/Skills'
 const Home = () => {
   return (
     <div>
+      <Head>
+        <title>Portfolio Muhammad Nurwibawanto</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Navbar />
       <Main />
       <About />
